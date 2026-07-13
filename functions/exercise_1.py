acf0f2fc19c695fb3d5f4496ee3ccc606c43619d
Yashus Rao <yashusrao@gmail.com>
2026-07-13 19:14:53 +0530

    Day 1 function basic exercises

---
+++ functions/exercise_1.py
@@ -0,0 +1,6 @@
+def farenheit_to_celsius(farenheit):
+    celsius = (farenheit - 32) * 5/9
+    print(celsius)
+farenheit_to_celsius(98.6)
+farenheit_to_celsius(32)
+farenheit_to_celsius(212)
---
+++ functions/exercise_2.py
@@ -0,0 +1,5 @@
+def describe_pet(name, animal_type="dog"):
+    print(f"{name} is a {animal_type}")
+describe_pet("Barney")
+describe_pet("Karma","chameleon")
+
