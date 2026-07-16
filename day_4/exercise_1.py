00c9b567e821af8c316dbb762e0c3ca90a17423b
Yashus Rao <yashusrao@gmail.com>
2026-07-16 18:47:22 +0530

    Day 4: set operations - union, intersection, difference

---
+++ day_4/exercise_1.py
@@ -0,0 +1,9 @@
+list1 = [1, 2, 3, 4, 5, 6]
+list2 = [4, 5, 6, 7, 8, 9]
+
+set1 = set(list1)
+set2 = set(list2)
+
+print("Union:", set1 | set2)          
+print("Intersection:", set1 & set2)  
+print("Difference:", set1 - set2)     
