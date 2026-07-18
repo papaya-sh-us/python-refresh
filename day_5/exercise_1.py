32f764460e4aece0e401140e6a17ab9bc2b65223
Yashus Rao <yashusrao@gmail.com>
2026-07-18 22:23:44 +0530

    add describe function to class Dog

--- day_5/exercise_1.py
+++ day_5/exercise_1.py
@@ -6,9 +6,14 @@ class Dog:
     def bark(self):
         print(self.name + " says 'Have you seen my updog?'")
 
+    def describe(self):
+        return self.name + " is a " + self.breed
+
 
 dog1 = Dog("Barney", "Labrador")
 dog2 = Dog("Max", "Poodle")
 
 dog1.bark()
 dog2.bark()
+
+print(dog1.describe())
