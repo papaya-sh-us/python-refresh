8c14cc7cf9a88a37423c5756e9f8670844c78c00
Yashus Rao <yashusrao@gmail.com>
2026-07-21 22:11:48 +0530

    create numpy arrays

---
+++ day_9/exercise_1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1, 2, 3])                 
+b = np.array([[1, 2, 3], [4, 5, 6]])  
+
+print(np.zeros(5))          
+print(np.ones((2, 3)))      
+print(np.arange(0, 10, 2))  
+print(np.linspace(0.0, 1.0, 5)) 
