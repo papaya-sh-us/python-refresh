c76d532c360deaf76f9a21e9ec4b5cddc162584d
Yashus Rao <yashusrao@gmail.com>
2026-07-22 22:47:33 +0530

    dot product with numpy

--- day_10/exercise_1.py
+++ day_10/exercise_1.py
@@ -8,3 +8,14 @@ def dot(v1, v2):
     return total
 
 print(dot(v1,v2))
+
+import numpy as np
+def numpy_dot(a,b):
+    print(np.dot(a, b))         
+    print(a @ b)                
+    print(np.sum(a * b))        
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+numpy_dot(a,b)
