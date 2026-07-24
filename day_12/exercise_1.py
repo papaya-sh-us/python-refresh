81d80178bb363247483ad62d77f9ea3cfb42b957
Yashus Rao <yashusrao@gmail.com>
2026-07-24 20:57:29 +0530

    verify using np.allclose

--- day_12/exercise_1.py
+++ day_12/exercise_1.py
@@ -7,3 +7,6 @@ eigenvalues, eigenvectors = np.linalg.eig(A)
 print(eigenvalues)         
 print(eigenvectors)
 
+lam = eigenvalues[0]                
+v = eigenvectors[:, 0]              
+print(np.allclose(A @ v, lam * v))  
